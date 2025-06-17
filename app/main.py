2194fb8c2803bbf3fe59f7d81949565472479cd8
atharva-k28 <atharva.kshirsagar2810@gmail.com>
2025-06-17 19:06:46 +0530

    TESTING FOR RENDER HOST WITHOUT HEAVY LIBRARIES

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 import uvicorn
-from fastapi import FastAPI, UploadFile, File
-from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
-from transformers import pipeline, AutoProcessor
-from PIL import Image
-from io import BytesIO
+#from fastapi import FastAPI, UploadFile, File
+#from fastapi.middleware.cors import CORSMiddleware
+#from pydantic import BaseModel
+#from transformers import pipeline, AutoProcessor
+#from PIL import Image
+#from io import BytesIO
 
 class Text(BaseModel):
     input_text:str
@@ -36,17 +36,19 @@ def process_text(data: Text):
 class url(BaseModel):
     img_url:str
 
-processor=AutoProcessor.from_pretrained("Salesforce/blip-image-captioning-base",use_fast=True)
-captioner = pipeline("image-to-text",model="Salesforce/blip-image-captioning-base",image_processor=processor)
+#processor=AutoProcessor.from_pretrained("Salesforce/blip-image-captioning-base",use_fast=True)
+#captioner = pipeline("image-to-text",model="Salesforce/blip-image-captioning-base",image_processor=processor)
 
 @app.post("/process-url")
 def process_url(data: url):
-    caption = captioner(data.img_url)
-    return{"caption":caption[0]['generated_text']}
+    #caption = captioner(data.img_url)
+    #return{"caption":caption[0]['generated_text']}
+    return {"caption":"test-caption"}
 
 @app.post("/process-image")
 async def process_image(file:UploadFile = File(...)):
-    contents = await file.read()
-    image = Image.open(BytesIO(contents))
-    caption = captioner(image)
-    return {"caption":caption[0]['generated_text']}
+    #contents = await file.read()
+    #image = Image.open(BytesIO(contents))
+    #caption = captioner(image)
+    #return {"caption":caption[0]['generated_text']}
+    return {"caption":"test-caption"}
